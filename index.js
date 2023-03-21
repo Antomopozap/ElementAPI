5f58f99a51ef903b942b061eb3b66b41cb4fb2b8
Antomopozap <85046221+Antomopozap@users.noreply.github.com>
2023-03-20 21:23:14 -0300

    El pepe

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const PORT = 8080;
 const db = require("megadb");
 const keys = new db.crearDB("keys");
 const users = new db.crearDB("users");
-
+const fs = require("fs");
+//?generate Key
 function generateApiKey(user) {
   const { randomBytes, createHash } = require("crypto");
   const apiKey = randomBytes(16).toString("hex");
@@ -21,7 +22,7 @@ function generateApiKey(user) {
     console.log(`Key generated succesfully, ${apiKey}`);
   }
 }
-
+//? Handling requests
 app.get("/api", (req, res) => {
   const { createHash } = require("crypto");
   const apiKey = req.query.apiKey;
@@ -30,11 +31,32 @@ app.get("/api", (req, res) => {
   const incryptedKey = createHash("md5").update(apiKey).digest("hex");
   if (!keys.has(incryptedKey)) res.sendStatus(400);
 
-  fetch("http://example.com/movies.json")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  res.send(readJSON("./elements.json"));
 });
 
 app.listen(PORT, () => {
   console.log("Server runnig at http://localhost:" + PORT);
 });
+
+function readJSON(path_file_name) {
+  if (!fs.existsSync(path_file_name))
+    throw new errorDB(
+      `El archivo ${path_file_name} no existe.`,
+      "DATABASE INVALIDA",
+      "readJSON",
+      path_file_name
+    );
+  var obj_data;
+  try {
+    let get_data = fs.readFileSync(path_file_name, "utf8");
+    obj_data = JSON.parse(get_data);
+  } catch (error) {
+    throw new errorDB(
+      `Ocurrio un problema al tratar de leer los datos del archivo ${path_file_name}, error: ${error}`,
+      "DATABASE INVALIDA",
+      "readJSON",
+      path_file_name
+    );
+  }
+  return obj_data;
+}
